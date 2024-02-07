b5110f9241f1a604bc5dde3c64c6ba273aae7465
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-02-07 20:38:02 +0600

    add design and post request

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import './App.css';
+import axiosApi from './axiosApi';
 
 const App = () => {
-  return (
-    <div>
+  const [urlText, setUrlText] = useState('');
 
+  const changeUrl = (event: ChangeEvent<HTMLInputElement>) => {
+    const {value} = event.target;
+    setUrlText(value);
+  };
+
+  const shortenUrl = async (event: FormEvent) => {
+    event.preventDefault();
+    await axiosApi.post('/links', {url: urlText});
+  };
+
+  return (
+    <div className="container">
+        <form onSubmit={shortenUrl} className="centerForm">
+          <div className="fieldBlock">
+            <h4 className="title">Shorten you link!</h4>
+            <input
+              onChange={changeUrl}
+              value={urlText}
+              className="field"
+              type="text"
+              placeholder="Enter your url"
+            />
+          </div>
+          <button type="submit" className="button">Shorten</button>
+        </form>
     </div>
   );
 };
