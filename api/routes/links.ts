ac8012ab80e8a250ce91acfa1930ab6ce246c71d
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-02-08 15:18:10 +0600

    made unique shorturl

--- api/routes/links.ts
+++ api/routes/links.ts
@@ -1,21 +1,31 @@
 import {Router} from "express";
 import Link from "../models/LinkModel";
 import {LinkData} from "../types";
+import mongoose from "mongoose";
 
 const linksRouter = Router();
 
 linksRouter.post('/', async (req, res, next) => {
   try {
     const originalUrl = req.body.url;
-    const symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let shortUrl = '';
+    const generateShortUrl = () => {
+      const symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      let shortUrl = '';
 
-    for (let i = 0; i < 6; i++) {
-      shortUrl += symbols[Math.floor(Math.random() * symbols.length)];
+      for (let i = 0; i < 6; i++) {
+        shortUrl += symbols[Math.floor(Math.random() * symbols.length)];
+      }
+      return shortUrl;
+    };
+
+    const data = await Link.find();
+    const existShortUrl = data.filter(item => item.shortUrl === generateShortUrl());
+    if (existShortUrl.length > 0) {
+      generateShortUrl();
     }
 
     const linkData: LinkData = {
-      shortUrl,
+      shortUrl: generateShortUrl(),
       originalUrl,
     };
 
